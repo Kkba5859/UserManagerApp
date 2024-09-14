a7958af9bfbeaa89894a679074e5e43bc4f5bc94
Artem Bychkov <kkba5859@gmail.com>
2024-09-14 15:00:17 +0400

    test2

--- obj/Debug/net8.0/UserManagerApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/UserManagerApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagerApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e53e59d2247999478c84a7aa8b66c5beff88d6f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62898e14c79193ad0e1eaedaf1cb17605967ec2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagerApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagerApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
